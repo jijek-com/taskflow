920d72275492a4c1dfabd46f00603445e0bd375c
jijek-com <mas.eug@yandex.ru>
2024-12-12 09:14:20 +0500

    added library ant design

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'taskflow';
+  isCollapsed = false;
+
+  constructor(private router: Router) {}
+
+  isCurrentRoute(route: string): boolean {
+    return this.router.isActive(route, true);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { NzLayoutModule } from "ng-zorro-antd/layout";
+import { NzMenuModule } from "ng-zorro-antd/menu";
+import { NZ_ICONS, NzIconModule } from "ng-zorro-antd/icon";
+import { NzBreadCrumbModule } from "ng-zorro-antd/breadcrumb";
+
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
+
+import { IconDefinition } from '@ant-design/icons-angular';
+import * as AllIcons from '@ant-design/icons-angular/icons';
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+
+const antDesignIcons = AllIcons as {
+  [key: string]: IconDefinition;
+};
+const icons: IconDefinition[] = Object.keys(antDesignIcons).map(key => antDesignIcons[key])
 
 @NgModule({
   declarations: [
@@ -10,9 +25,14 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    NzLayoutModule,
+    NzMenuModule,
+    NzIconModule,
+    NzBreadCrumbModule
   ],
-  providers: [],
+  providers: [{ provide: NZ_ICONS, useValue: icons } ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
